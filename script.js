b133edc8d0bc592bbd453ca68657b11559f897a9
James Inglis <james.a.i.inglis@gmail.com>
2017-06-22 15:32:47 +1200

    updating files

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//English to Maori Translator
+//Maori Quote Generator
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+
+var button=document.getElementById("button");
+
+button.addEventListener(click, function (c)) {
+  $.ajax({
+    url: "https://eda-te-reo.herokuapp.com/api/proverbs";
+  })
+  .done(function (data)){
+    var authorDiv = document.getElementById("author");
+    var quoteDiv = document.getElementById("quote");
+    authorDiv.innerHTML = data.translation;
+    quoteDiv.innerHTML = data.source;
+  };
+};
