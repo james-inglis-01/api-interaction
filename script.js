d15fd97a6e1028c25383993d12fc7665d8fe12b3
James Inglis <james.a.i.inglis@gmail.com>
2017-06-22 15:56:40 +1200

    updating

--- script.js
+++ script.js
@@ -3,16 +3,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   console.log("DOM fully loaded and parsed");
 
-var button=document.getElementById("button");
+  var button = document.getElementById("button");
+  //add listener event listener.
+  button.addEventListener("click", function(e) {
+    //request to API -> AJAX, Native HTTP req or FETCH in browser or JQuery.
+    $.ajax({
+      url: "https://eda-te-reo.herokuapp.com/api/proverbs"
+    }).done(function(data) {
+      console.log(data);
+      // var quoteobj=JSON.parse(data);
+      var authorDiv = document.getElementById("author");
+      var quoteDiv = document.getElementById("quote");
+      authorDiv.innerHTML = data.translation;
+      quoteDiv.innerHTML = data.source;
+    });
 
-button.addEventListener(click, function (c)) {
-  $.ajax({
-    url: "https://eda-te-reo.herokuapp.com/api/proverbs";
-  })
-  .done(function (data)){
-    var authorDiv = document.getElementById("author");
-    var quoteDiv = document.getElementById("quote");
-    authorDiv.innerHTML = data.translation;
-    quoteDiv.innerHTML = data.source;
-  };
-};
+    //put results into div
+  });
+});
